ce98459b17b79ce495f1b76989ba9df612724155
Miguelo0098 <i62rarum@uco.es>
2018-10-17 17:28:37 +0200

    terminado index.php

--- practicas/practica2/index.php
+++ practicas/practica2/index.php
@@ -8,25 +8,9 @@
 
 <?php
 	/* Incluyo las funciones de consulta de la base de datos */
-	require_once('aquivaelarchivodephp.php');
+	require_once('employeefunctions.php');
 
-	$servername = ;
-	$username = ;
-	$password = ;
-	$dbname = ;
-
-	$conn = new mysqli($servername, $username, $password);
-	$conn->select_db($dbname);
-
-	if ($conn->connect_error) {
-	    die("Conexion fallida: " . $conn->connect_error);
-	}
-
-	$sql = "select * from Usuarios";
-	$query = $conn->query($sql);
-
-	/* Crea un nuevo objeto para llamar a las consultas */
-	#$query = new databaseQueries();
+	$query = new EmployeeQueries();
 
 	/* Comprueba que se ha realizado bien la conexion a la base de datos */
 	if(empty($query->dbc)){
@@ -36,23 +20,27 @@
 
 	/* Obtengo el listado de empleados */
 echo <<<_END
-	<h1><b>TeaMoja Enterprise Employee Database</b></h1>
-	<p class="warning"><b>WARNING</b>: Esta página contiene información de<br><b>ALTO SECRETO</b> y su difusión está penada con<br><b>CADENA PERPÉTUA</b>.</p>
+		<h1><b>TeaMoja Enterprise Employee Database</b></h1>
 
-	<div>
-		<h3>Lista de empleados</h3>
-	<ul>
+		<h3 allign='center'>Lista de empleados</h3>
+		<table align='center'>
+			<tr align='center'>
+				<th>ID</th>
+				<th>NICK</th>
 _END;
 
-	while($employees = $query->getEmployees()) {
-		echo' <li><a href="employee.php?q='. $employees["nombreCifrado"] .'">' . $employees["uwu"]. '</a></li>';
+	$employees = $query->getEmployees();
+	foreach($employees as $employee) {
+		echo <<<_END
+			<tr allign='center'>
+				<td>$employee[ID]</td>
+				<td>$employee[NICK]</td>
+_END
 	}
-
-	echo "</ul>
-	</div> ";
-
-	}
-echo <<<_END
+	echo <<<_END
+		</table>
+_END
+	echo <<<_END
 	<p id="copyright">This site uses cookies to deliver our services and to show you relevant ads and job listings. By using our site, you acknowledge that you have read and understand our Cookie Policy, Privacy Policy, and our Terms of Service.</p>
 _END;
 
