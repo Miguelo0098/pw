3f8f39a5c84c60b34ba9ec27ddcd7fb557acf7f3
Miguelo0098 <i62rarum@uco.es>
2018-10-17 17:37:45 +0200

    rehecha copia server

--- httdocs-copiaseguridadxampp/index.php
+++ httdocs-copiaseguridadxampp/index.php
@@ -1,11 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>TEAMOJA Enterprise</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
+	/* Incluyo las funciones de consulta de la base de datos */
+	require_once('employeefunctions.php');
+
+	$query = new EmployeeQueries();
+
+	/* Comprueba que se ha realizado bien la conexion a la base de datos */
+	if(empty($query->dbc)){
+		echo "<h3 align='center'>Ha habido un problema al conectar con la base de datos. Por favor, vuelva mas tarde. </h3></br>";
+		die();
 	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+
+	/* Obtengo el listado de empleados */
+echo <<<_END
+		<h1><b>TeaMoja Enterprise Employee Database</b></h1>
+
+		<h3 allign='center'>Lista de empleados</h3>
+		<table align='center'>
+			<tr align='center'>
+				<th>ID</th>
+				<th>NICK</th>
+_END;
+
+	$employees = $query->getEmployees();
+	foreach($employees as $employee) {
+		echo <<<_END
+			<tr allign='center'>
+				<td>$employee[ID]</td>
+				<td>$employee[NICK]</td>
+_END
+	}
+	echo <<<_END
+		</table>
+_END
+	echo <<<_END
+	<p id="copyright">This site uses cookies to deliver our services and to show you relevant ads and job listings. By using our site, you acknowledge that you have read and understand our Cookie Policy, Privacy Policy, and our Terms of Service.</p>
+_END;
+
+	$conn->close();
+
 ?>
-Something is wrong with the XAMPP installation :-(
+</body>
+</html>
