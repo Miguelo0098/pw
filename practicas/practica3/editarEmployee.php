5fa8cec925a5f7627b8eea80689d24241f77a00e
Miguelo0098 <i62rarum@uco.es>
2018-11-12 11:37:21 +0100

    copiado contenido añadir a editar

--- practicas/practica3/editarEmployee.php
+++ practicas/practica3/editarEmployee.php
@@ -8,88 +8,94 @@
 <body>
 
 <?php
-	//Declaro variables
-	$ID = $_GET["employee"];
 
-	require_once('employeefunctions.php');
-	$query = new EmployeeQueries();
+require_once('employeefunctions.php');
 
-	// Comprueba que se ha realizado bien la conexion a la base de datos
+/* Create queries object */
+$q = new EmployeeQueries();
+if(empty($q->dbc)){
+    echo "<h3 align='center'>¡Error!: No se pudo establecer la conexión con la base de datos.</h3><br/>";
+    die();
+}
 
-	if(empty($query->dbc)){
-		echo "<h3 align='center'>Ha habido un problema al conectar con la base de datos. Por favor, vuelva mas tarde. </h3></br>";
-		die();
-	}
+/* Check is the book is going to be modified */
+if (isset($_POST['addagent'])){
 
-	// Crea un nuevo objeto para llamar a las consultas
-	$databaseUser = $query->getEmployee($ID);
+    //Check if cancel button has been selected
 
-	//**A TENER EN CUENTA** -> Hay que ver como poner en opciones que no sean bloques de texto, la previsualizacion del valor anterior al cambio
+        $check = true;
+
+        //Get the new book values
+        $newid = $q->maxIdEmployee();
+        $agent[0] = $newid[0] + 1;
+        $agent[1] = $_POST['username'];
+        $agent[2] = $_POST['nick'];
+        $agent[3] = $_POST['gender'];
+        $agent[4] = $_POST['edad'];
+        $agent[5] = $_POST['photo'];
+        $agent[6] = $_POST['specialty'];
+        $agent[7] = $_POST['contactInfo'];
+
+
+        if($check){
+            $status = $q->addEmployee($agent);
+            //If correctly added, go to books page
+            if($status){
+                header('Location: /index.php');
+            }
+            else{
+                echo "<h3 align='center' style='color: red'>An error ocurred. Please try again.</h3><br>";
+            }
+        }else{
+            echo "<h3 align='center' style='color: red'>Please check the fields and try again.</h3><br>";
+        }
+
+
+
+}
+
 echo <<<_END
 	<body>
 		<img id="uco" src="./pics/índice.jpeg" alt="UCO LOGO">
 		<h1><b>Special Agents Database</b></h1>
 		<h3>Creacion de un nuevo miembro</h3>
-		<form>
+		<form action="addemployee.php" method="post">
 			Name (encrypted):<br>
-			<input type="text" name="username"><br>
+			<input type="text" name="username" required><br>
 
 			<br>Nick:<br>
-			<input type="text" name="nick"><br>	
+			<input type="text" name="nick" required><br>
 
 			<br>Sexo:<br>
-			<input type="radio" name="gender" value="male"> Hombre<br>
-			<input type="radio" name="gender" value="female"> Mujer<br>
-			<input type="radio" name="gender" value="other"> Otro<br>
-
+			<input type="radio" name="gender" value="Hombre" checked> Hombre<br>
+			<input type="radio" name="gender" value="Mujer"> Mujer<br>
+			<input type="radio" name="gender" value="Otro"> Otro<br>
+			<br>Edad:<br>
+			<select name="edad">
+_END;
+			for ($i=16; $i < 100; $i++) {
+				echo "<option value='$i'>$i</option>";
+			}
+echo <<<_END
+			</select>
 			<br>Especialidad:<br>
-			<input type="checkbox" name="specialty1" value=""> Sigiloso<br>
-			<input type="checkbox" name="specialty2" value=""> Suertudo<br>
-			<input type="checkbox" name="specialty3" value=""> Rico<br>
-			<input type="checkbox" name="specialty4" value=""> Persuasivo<br>
-			<input type="checkbox" name="specialty5" value=""> Agil<br>
-			<input type="checkbox" name="specialty6" value=""> Inteligente <br>
-	
+			<input type="text" name="specialty" value="" required><br>
+			<br>Foto<br>
+			<input type="text" name="photo"><br>
 			<br>Direccion de contacto:<br>
-			<input type="text" name="contactInfo"><br>
+			<input type="text" name="contactInfo" required><br>
+			<br>
+            <input type="checkbox" name="terms" required>Acepto los términos y condiciones<br>
+			<input type="submit" name="addagent" value="Añadir">
 		</form>
 	<a id="back" href="./index.php">Atrás</a>
   	<p id="cookies">This site uses cookies to deliver our services. By using our site, you acknowledge that you have read and understand our Cookie Policy, Privacy Policy, and our Terms of Service.</p>
 	</body>
 _END;
 
-	//Compruebo que se ha introducido todo
-	if(!isset($_POST('username')) || 
-		!isset($_POST('nick')) || 
-		!isset($_POST('gender')) || 
-		!isset($_POST('contactInfo')) ){
-		//Hasta aqui compruebo todos los apartados salvo los de especialidad
-		
-		if(isset($_POST('specialty1')) ||
-			isset($_POST('specialty2')) ||
-			isset($_POST('specialty3')) ||
-			isset($_POST('specialty4')) ||
-			isset($_POST('specialty5')) ||
-			isset($_POST('specialty6'))){
-			
-			//Con que se introduzca uno de ellos valdra.
-			//**A TENER EN CUENTA** -> Hay que ver como metemos las especialidades! (deberia de ser alguna clase de vector o array)
-			$sql = "INSERT INTO EMPLOYEE (ID, NOMBRE, NICK, EDAD, SEXO, ESPECIALIDAD, CONTACTO) VALUES ($newID, $name, $nick, $age, $gender, $specialty, $contactInfo)"
-	
-			if ($conn->query($sql) == TRUE) {
-		    	echo "Se ha introducido correctamente al agente,";
-			} else {
-	    	echo "Ha habido un error introduciendo al agente: " . $conn->error;
-			}
 
-		}else{
-				//No se ha introducido ningun apartado de especialidad
-				echo "Hay que rellenar todos los apartados!"
-		}
-	}else{
-			//No se ha introducido alguno de los apartados
-			echo "Hay que rellenar todos los apartados!"
-	}
+	//Compruebo que se ha introducido todo
+
 
 ?>
 
