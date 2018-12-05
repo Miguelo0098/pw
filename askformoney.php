b3a04e25bab5ce1277d48a59766bb77338d4f0dc
Miguelo0098 <i62rarum@uco.es>
2018-12-05 10:03:55 +0100

    preparando el terreno

---
+++ askformoney.php
@@ -0,0 +1,43 @@
+<?php
+	require_once('session.php');
+ ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>UCO Agents</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+<?php
+	/* Incluyo las funciones de consulta de la base de datos */
+	require_once('employeefunctions.php');
+
+
+	$query = new EmployeeQueries();
+
+	/* Comprueba que se ha realizado bien la conexion a la base de datos */
+	if(empty($query->dbc)){
+		echo "<h3 align='center'>Ha habido un problema al conectar con la base de datos. Por favor, vuelva mas tarde. </h3></br>";
+		die();
+	}
+
+	if (isset($_SESSION['expire']) && time() >= $_SESSION['expire']) {
+		session_destroy();
+		echo "<h3 align='center'>Su sesión ha caducado. Volviendo a la página principal.</h3><br/>";
+	    header("refresh: 5; http://localhost:80/index.php");
+		exit();
+	}
+
+	/* Obtengo el listado de empleados */
+echo <<<_END
+		<img id="uco" src="./pics/índice.jpeg" alt="UCO LOGO">
+		<div id='login'>
+		<h1><b>Special Agents Database</b></h1>
+_END;
+	if (LOGGED) {
+		echo "<h5 align='right'>Bienvenido ".USERNAME." | <a href='logout.php'>Cerrar Sesión</a></h5>";
+	}else{
+		echo "<h5 align='right'><a href='login.php'>Iniciar Sesión | Registrarse</a></h5>";
+	}
--- practicas/practica4/index.php
+++ practicas/practica4/index.php
@@ -41,6 +41,9 @@ _END;
 	}else{
 		echo "<h5 align='right'><a href='login.php'>Iniciar Sesión | Registrarse</a></h5>";
 	}
+	if(LOGGED && !ADMIN){
+		echo "<h5 align='right'><a href='askformoney.php'>Solicitar Aumento</a></h5>";
+	}
 
 echo <<<_END
 		</div>
@@ -71,6 +74,9 @@ _END;
 				<a href="editarEmployee.php?employee=$employee[ID]"><img id="icono" src="./pics/edit.png" alt="Editar"></a>
 				<a href="borrarEmployee.php?employee=$employee[ID]"><img id="icono" src="./pics/delete.png" alt="Borrar"></a>
 _END;
+				if ($employee[AUMENTO]) {
+			echo "<a href='confirmaraumento.php?employee=$employee[ID]'><img id='icono' src='./pics/dolar.jpg' alt='Revisar Salario'></a>";
+				}
 			}
 			echo "</td>";
 		}else{
